0bd419ee8974ee7989359fd9aa6ef31206559fb8
yamertyourgod <yamertyourgod@gmail.com>
2015-12-11 09:24:03 +0200

    Citadel and Modele classes

--- UnityProject-4/Assets/Scripts/SteamCitadel.cs
+++ UnityProject-4/Assets/Scripts/SteamCitadel.cs
@@ -1,15 +1,61 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class SteamCitadel : MonoBehaviour {
+public class SteamCitadel : MonoBehaviour
+{
+    public int CurrrentTotalAmontOfModules;
+    public int MaxTotalAmontOfModules;
+    public float CurrntTotalStorage;
+    public float MaxTotalStorage;
+    public int TotalRadarRange;
+    public int TotalRadarAcc;
+    public float TotalMass;
+    public float TotalForce;
+    public float TotalConsumption;
+    public float GloabalTotalSpeed;
+    public float TacticalTotalSpeedFactor;
+    public float TotalAcceleration;
+    public float TotalTurnSpeed;
+    public int MaxTotalControl;
+    public int AvailableTotalControl;
+    public int TotalSMAmount;
+    public int MaxTotalSchemeAmount;
+    public int CurrentTotalSchemeAmount;
+    public EnumCitadelRole Role;
+    public float TotalRecoveryMetal;
+    public float TotalRecoveryParts;
+    public float TotalRecoveryEnergy;
+    public float MaxTotalShield;
+    public float CurrentTotalShield;
+    public float PhysTotalConShieldArm;
+    public float EnergyTotalConShieldArm;
+    public float HeatTotalConShieldArm;
+    public float TotalShieldPower;
+    public float OnlineFinalTotalShieldRegen;
+    public float OfflineFinalTotalShieldRegen;
+    public float TotalShieldTreshHold;
+    public float TotalConstructionSpeed;
+    public int MaxStorageParts;
+    public int CurrentStorageParts;
+    public float TotalConversionSpeed;
+    public float TotalConversionEff;
+    public float TotalPotential;
+    // Use this for initialization
+    void Start()
+    {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public enum EnumCitadelRole
+    {
+        SMControl,
+        PilotControl,
+        SelfControl
+    }
 }
---
+++ UnityProject-4/Assets/Scripts/SteamCitadelConstrutor.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SteamCitadelConstrutor : MonoBehaviour {
+    public GameObject Bridge;
+    public GameObject Constructor;
+    public GameObject Engine;
+    public GameObject Radar;
+    public GameObject Bridge;
+    public GameObject Storage;
+    public GameObject Weapon;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
