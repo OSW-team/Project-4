7253aeda0d71efc1f02b4db6101f59bb80977167
yamert <yamertwork,ail@hotmail.com>
2016-01-12 17:12:36 +0200

    WheelColliderTest

---
+++ UnityProject-4/Assets/Scripts/TestWheelColider.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestWheelColider : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	    if (Input.GetKey(KeyCode.W))
+	    {
+	        transform.position += Vector3.forward*5;
+	    }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += Vector3.back * 5;
+        }
+    }
+}
