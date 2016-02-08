ea7aeb77f3a4e3cdfd380a607bc2d9cd6041284b
yamert <yamertwork,ail@hotmail.com>
2016-02-08 17:58:49 +0200

    сцена для теста

---
+++ UnityProject-4/Assets/Scripts/Test/BoxMovement.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoxMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.localPosition += Vector3.forward * 0.10f;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.localPosition += Vector3.back * 0.10f;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.localPosition += Vector3.right * 0.10f;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.localPosition += Vector3.left * 0.10f;
+        }
+    }
+}
