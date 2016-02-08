d20d33c82b4755a28b8156eca01bd5b9f170a103
SallahAdDin <romaogo@mail.ru>
2016-02-08 17:47:26 +0200

    Упрощение колёс

---
+++ UnityProject-4/Assets/FitRotation.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FitRotation : MonoBehaviour {
+	public Transform[] bridges;
+	public float angularSpeed;
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 result = Vector3.zero;
+		foreach (Transform bridge in bridges) {
+			result += bridge.up;
+		}
+		transform.rotation = Quaternion.RotateTowards (transform.rotation, Quaternion.LookRotation (Vector3.Cross (transform.right, result), result), angularSpeed*Time.deltaTime);
+	
+	}
+}
