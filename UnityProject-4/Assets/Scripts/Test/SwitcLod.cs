ae2c802c550cba96fde9923f242eeefbc3e3787e
yamert <yamertwork,ail@hotmail.com>
2016-02-16 13:56:51 +0200

    тесты

---
+++ UnityProject-4/Assets/Scripts/Test/SwitcLod.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwitcLod : MonoBehaviour {
+    private bool show;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+        if(Input.GetKeyDown(KeyCode.B))
+        {
+            transform.GetChild(1).gameObject.SetActive(show);
+            transform.GetChild(0).gameObject.SetActive(!show);
+            show = !show;
+        }
+	}
+}
